129edc8c2f94b9aacf7b296b4425217d8427e48d
Erdoğan <45108900+erdogansireci@users.noreply.github.com>
2024-05-31 00:03:30 +0300

    enum fix

--- Shared/Enums.js
+++ Shared/Enums.js
@@ -1,5 +1,5 @@
 
-// Servise erişilen platformun tipini verir
+// Platforms which can access to service
 const PlatformType = Object.freeze({"WEB":1, "DESKTOP":2, "ANDROID":3, "IOS":4, "POSTMAN":999 });
 
 module.exports = PlatformType;
